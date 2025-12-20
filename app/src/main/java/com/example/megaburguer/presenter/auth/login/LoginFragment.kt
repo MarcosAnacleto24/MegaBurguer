db4db729d9e7fc5925d7c5d6c52fb55da4eed6e0
Marcos Anacleto <marcosanacleto66@gmail.com>
2025-12-20 16:19:03 -0300

    Criar layout login

---
+++ app/src/main/java/com/example/megaburguer/presenter/auth/login/LoginFragment.kt
@@ -0,0 +1,34 @@
+package com.example.megaburguer.presenter.auth.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.megaburguer.R
+import com.example.megaburguer.databinding.FragmentLoginBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
--- app/src/main/java/com/example/megaburguer/presenter/splash/SplashFragment.kt
+++ app/src/main/java/com/example/megaburguer/presenter/splash/SplashFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.example.megaburguer.R
 import com.example.megaburguer.databinding.FragmentSplashBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SplashFragment : Fragment() {
@@ -25,6 +29,16 @@ class SplashFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            delay(3000)
+            checkAuth()
+        }
+    }
+
+    private fun checkAuth() {
+
+        findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
     }
 
     override fun onDestroyView() {
